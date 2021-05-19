24d02905c4524922f84abfa035cc11230df3134d
TamaraMayer <tamara-mayer@outlook.com>
2021-05-19 15:54:56 +0200

    TestMethode für 1 Zahl hinzugefügt

--- String_Calculator/StringCalculator/UnitTest1.cs
+++ String_Calculator/StringCalculator/UnitTest1.cs
@@ -11,5 +11,12 @@ namespace StringCalculator.Test
         {
            Assert.AreEqual(0,String_Calculator.StringCalculator.Add(string.Empty));
         }
+
+        [DataTestMethod]
+        [DataRow ("1")]
+        public void Add_Should_Return_The_Given_Number(string numberString)
+        {
+            Assert.AreEqual(numberString.ToString(), String_Calculator.StringCalculator.Add(numberString));
+        }
     }
 }
