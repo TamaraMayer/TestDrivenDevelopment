72ac5270f74efc33f486b5ab1a0400219ce4d613
TamaraMayer <tamara-mayer@outlook.com>
2021-05-19 16:25:22 +0200

    neuer test hinzugefügt "1\n2,4"

--- String_Calculator/StringCalculator/UnitTest1.cs
+++ String_Calculator/StringCalculator/UnitTest1.cs
@@ -44,6 +44,7 @@ namespace StringCalculator.Test
 
         [DataTestMethod]
         [DataRow ("1\n2,3",6)]
+        [DataRow("1\n2,4", 7)]
         public void Add_Should_Return_The_added_numbers_no_matter_how_many_numbers_numbers_may_be_split_With_newlines_or_commas(string numberString, int solution)
         {
             Assert.AreEqual(solution, String_Calculator.StringCalculator.Add(numberString));
