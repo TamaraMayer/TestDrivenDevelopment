40bed5efa356bcf93e721552b32a118dbbd53c88
TamaraMayer <tamara-mayer@outlook.com>
2021-05-19 16:12:17 +0200

    test case für mehr als 2 zahlen hinzugefügt "1,2,3"

--- String_Calculator/StringCalculator/UnitTest1.cs
+++ String_Calculator/StringCalculator/UnitTest1.cs
@@ -27,7 +27,14 @@ namespace StringCalculator.Test
         [DataRow ("1,2", 3)]
         [DataRow("2,2", 4)]
         [DataRow ("1,3",4)]
-        public void Add_Should_Return_The_added_numbers(string numberString, int solution)
+        public void Add_Should_Return_Two_Numers_added(string numberString, int solution)
+        {
+            Assert.AreEqual(solution, String_Calculator.StringCalculator.Add(numberString));
+        }
+
+        [DataTestMethod]
+        [DataRow("1,2,3", 6)]
+        public void Add_Should_Return_The_added_numbers_no_matter_how_many_numbers(string numberString, int solution)
         {
             Assert.AreEqual(solution, String_Calculator.StringCalculator.Add(numberString));
         }
