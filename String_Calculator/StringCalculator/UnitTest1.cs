ab70e49c57cd26805e4cbed666b882fec5a09861
TamaraMayer <tamara-mayer@outlook.com>
2021-05-19 16:14:06 +0200

    neue test case hinzugefügt "1,2,4"

--- String_Calculator/StringCalculator/UnitTest1.cs
+++ String_Calculator/StringCalculator/UnitTest1.cs
@@ -34,6 +34,8 @@ namespace StringCalculator.Test
 
         [DataTestMethod]
         [DataRow("1,2,3", 6)]
+        [DataRow("1,2,4", 7)]
+
         public void Add_Should_Return_The_added_numbers_no_matter_how_many_numbers(string numberString, int solution)
         {
             Assert.AreEqual(solution, String_Calculator.StringCalculator.Add(numberString));
