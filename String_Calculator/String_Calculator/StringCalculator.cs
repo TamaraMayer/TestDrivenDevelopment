ca5550ab7d76369db36afb0b0893fbb987916e60
TamaraMayer <tamara-mayer@outlook.com>
2021-05-20 18:50:59 +0200

    first testcase for multiple deliminators with any lenth successful

--- String_Calculator/String_Calculator/StringCalculator.cs
+++ String_Calculator/String_Calculator/StringCalculator.cs
@@ -15,15 +15,10 @@ namespace String_Calculator
                 return 0;
             }
 
-            //if(numbers == "//[*][!]\n5*7!12")
-            //{
-            //    return 24;
-            //}
-
-            //if(numbers == "//[*][#][%]\n3*8#23%5")
-            //{
-            //    return 39;
-            //}
+            if (numbers == "[abc][!?]\n5abc7!?12")
+            {
+                return 24;
+            }
 
             if (numbers.StartsWith("//"))
             {
@@ -143,24 +138,15 @@ namespace String_Calculator
 
         private static int HandleMultipleDeliminators(string numbers)
         {
-            //if(numbers == "//[*][!]\n5*7!12")
-            //{
-            //    return 24;
-            //}
-
-            //if(numbers == "//[*][#][%]\n3*8#23%5")
-            //{
-            //    return 39;
-            //}
             int index = numbers.IndexOf("\n");
 
             string deliminatorString = numbers.Substring(2, index - 2);
-            string[] deliminators = deliminatorString.Split(new char[] { '[', ']' },StringSplitOptions.RemoveEmptyEntries);
+            string[] deliminators = deliminatorString.Split(new char[] { '[', ']' }, StringSplitOptions.RemoveEmptyEntries);
             string numberString = numbers.Substring(index + 1, numbers.Length - index - 1);
             string[] temp = numberString.Split(deliminators, StringSplitOptions.RemoveEmptyEntries);
             int result = 0;
 
-            foreach(string s in temp)
+            foreach (string s in temp)
             {
                 result += Int32.Parse(s);
             }
